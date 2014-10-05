962af0c8a94bbe2ce9aea2e5bebbff87c17de1be
Andrew Fowler <andrew.fowler@netnook.net>
2014-10-05 11:48:25 +0200

    Predicate: source ordering

--- src/main/java/net/netnook/qpeg/expressions/Predicate.java
+++ src/main/java/net/netnook/qpeg/expressions/Predicate.java
@@ -2,6 +2,41 @@ package net.netnook.qpeg.expressions;
 
 public final class Predicate extends SimpleExpression {
 
+	public static Builder match(ParsingExpressionBuilder expression) {
+		return new Builder().expression(expression, false);
+	}
+
+	public static Builder not(ParsingExpressionBuilder expression) {
+		return new Builder().expression(expression, true);
+	}
+
+
+	public static class Builder extends ParsingExpressionBuilderBase {
+		private ParsingExpressionBuilder expression;
+		private boolean invert;
+
+		public Builder expression(ParsingExpressionBuilder expression, boolean invert) {
+			this.expression = expression;
+			this.invert = invert;
+			return this;
+		}
+
+		@Override
+		public Builder ignore() {
+			throw new UnsupportedOperationException();
+		}
+
+		@Override
+		public Builder onSuccess(OnSuccessHandler onSuccess) {
+			throw new UnsupportedOperationException();
+		}
+
+		@Override
+		public Predicate build(BuildContext ctxt) {
+			return new Predicate(this, expression.build(ctxt));
+		}
+	}
+
 	protected final ParsingExpression expression;
 	protected final boolean invert;
 
@@ -32,39 +67,5 @@ public final class Predicate extends SimpleExpression {
 		return invert  //
 				? "!(" + expression.buildGrammar() + ")" //
 				: "&(" + expression.buildGrammar() + ")";
-	}
-
-	public static Builder match(ParsingExpressionBuilder expression) {
-		return new Builder().expression(expression, false);
-	}
-
-	public static Builder not(ParsingExpressionBuilder expression) {
-		return new Builder().expression(expression, true);
-	}
-
-	public static class Builder extends ParsingExpressionBuilderBase {
-		private ParsingExpressionBuilder expression;
-		private boolean invert;
-
-		public Builder expression(ParsingExpressionBuilder expression, boolean invert) {
-			this.expression = expression;
-			this.invert = invert;
-			return this;
-		}
-
-		@Override
-		public Builder ignore() {
-			throw new UnsupportedOperationException();
-		}
-
-		@Override
-		public Builder onSuccess(OnSuccessHandler onSuccess) {
-			throw new UnsupportedOperationException();
-		}
-
-		@Override
-		public Predicate build(BuildContext ctxt) {
-			return new Predicate(this, expression.build(ctxt));
-		}
 	}
 }
