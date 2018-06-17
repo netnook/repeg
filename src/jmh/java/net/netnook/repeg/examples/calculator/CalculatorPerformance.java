ba4c4f5eb726ea78c73b561a4e28e4f93a65b577
Andrew Fowler <andrew.fowler@netnook.net>
2018-06-17 11:57:39 +0100

    Added jmh params

--- src/jmh/java/net/netnook/repeg/examples/calculator/CalculatorPerformance.java
+++ src/jmh/java/net/netnook/repeg/examples/calculator/CalculatorPerformance.java
@@ -1,8 +1,12 @@
 package net.netnook.repeg.examples.calculator;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Measurement;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Warmup;
 
 import net.netnook.repeg.Parser;
 
@@ -17,6 +21,8 @@ public class CalculatorPerformance {
 	}
 
 	@Benchmark
+	@Warmup(iterations = 3, time = 1000, timeUnit = TimeUnit.MILLISECONDS)
+	@Measurement(iterations = 5, time = 2000, timeUnit = TimeUnit.MILLISECONDS)
 	public Object run(BenchmarkState state) {
 		Integer result = state.parser.parse(state.input);
 		if (!result.equals(state.expect)) {
--- src/jmh/java/net/netnook/repeg/examples/template/TemplatePerformance.java
+++ src/jmh/java/net/netnook/repeg/examples/template/TemplatePerformance.java
@@ -1,8 +1,12 @@
 package net.netnook.repeg.examples.template;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Measurement;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Warmup;
 
 import net.netnook.repeg.Parser;
 import net.netnook.repeg.examples._utils.ResourceLoader;
@@ -17,6 +21,8 @@ public class TemplatePerformance {
 	}
 
 	@Benchmark
+	@Warmup(iterations = 3, time = 1000, timeUnit = TimeUnit.MILLISECONDS)
+	@Measurement(iterations = 5, time = 2000, timeUnit = TimeUnit.MILLISECONDS)
 	public Object run(BenchmarkState state) {
 		Template template = state.parser.parse(state.input);
 		if (template == null) {
